8d19f9902107b340424b5d4fe9c9b6d0707cbf2b
Jenny Sun <xmissj96@gmail.com>
2019-11-21 23:22:06 +0100

    Changes: Error message to make the app more user-friendly. Optional in assignment.

--- app/src/main/java/com/example/gamebacklog/AddActivity.kt
+++ app/src/main/java/com/example/gamebacklog/AddActivity.kt
@@ -27,6 +27,9 @@ class AddActivity : AppCompatActivity() {
 
     private fun onAddClick() {
         val MONTHS = arrayListOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+        val dayRange = 1..31
+        val monthRange = 1..12
+        val yearRange = 1900..2025
 
         var title = etTitle.title.text.toString()
         var platform = etPlatform.platform.text.toString()
@@ -37,15 +40,28 @@ class AddActivity : AppCompatActivity() {
         // Check whether the input isn't empty.
         if (title.isNotBlank() && platform.isNotBlank() && day.isNotBlank() && month.isNotBlank() && year.isNotBlank()) {
 
-            // Fix error with the array length.
-            val game = Game(title, platform, day.toInt(), MONTHS[month.toInt()-1], year.toInt())
+            // Check whether the dates are in certain range and are valid.
+            if (day.toInt() in dayRange && month.toInt() in monthRange && year.toInt() in yearRange) {
+                // Fix error with the array length.
+                val game =
+                    Game(title, platform, day.toInt(), MONTHS[month.toInt() - 1], year.toInt())
 
-            val resultIntent = Intent()
-            resultIntent.putExtra(EXTRA_GAME, game)
-            setResult(Activity.RESULT_OK, resultIntent)
-            finish()
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_GAME, game)
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish()
+            } else {
+                Toast.makeText(this,"Please fill in a valid date.", Toast.LENGTH_LONG).show()
+            }
+
+        } else if (!title.isNotBlank()){
+            Toast.makeText(this,"Please fill in a title.", Toast.LENGTH_LONG).show()
+        } else if (!platform.isNotBlank()){
+            Toast.makeText(this,"Please fill in a platform.", Toast.LENGTH_LONG).show()
+        } else if (!day.isNotBlank() || !month.isNotBlank() || !year.isNotBlank()){
+            Toast.makeText(this,"Please fill in a date.", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(this,"The game cannot be empty, please fill in the empty fields!", Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"Multiple fields are empty. Please fill them in.", Toast.LENGTH_LONG).show()
         }
     }
 
